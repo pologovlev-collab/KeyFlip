391c27ccc45691c5bfea0a3e4ed6254ca8ec8f01
Lev <pologovlev2007@gmail.com>
2026-08-10 00:08:38 +0400

    test: report failures without Windows crash dialogs

--- tests/KeyFlip.Tests/Program.cs
+++ tests/KeyFlip.Tests/Program.cs
@@ -1,20 +1,29 @@
 using KeyFlip;
 using KeyFlip.Tests;
 
-var tests = new LayoutConverterTests();
-tests.Run();
-var nativeInteropTests = new NativeInteropTests();
-nativeInteropTests.Run();
-var inputSimulatorTests = new InputSimulatorTests();
-inputSimulatorTests.Run();
-var conversionGuardTests = new ConversionGuardTests();
-conversionGuardTests.Run();
-var clipboardSnapshotTests = new ClipboardSnapshotTests();
-clipboardSnapshotTests.Run();
-var settingsServiceTests = new SettingsServiceTests();
-settingsServiceTests.Run();
-var focusedContextClassifierTests = new FocusedContextClassifierTests();
-focusedContextClassifierTests.Run();
-var fileNameConverterTests = new FileNameConverterTests();
-fileNameConverterTests.Run();
-Console.WriteLine($"Passed: {tests.Passed + nativeInteropTests.Passed + inputSimulatorTests.Passed + conversionGuardTests.Passed + clipboardSnapshotTests.Passed + settingsServiceTests.Passed + focusedContextClassifierTests.Passed + fileNameConverterTests.Passed}");
+try
+{
+    var tests = new LayoutConverterTests();
+    tests.Run();
+    var nativeInteropTests = new NativeInteropTests();
+    nativeInteropTests.Run();
+    var inputSimulatorTests = new InputSimulatorTests();
+    inputSimulatorTests.Run();
+    var conversionGuardTests = new ConversionGuardTests();
+    conversionGuardTests.Run();
+    var clipboardSnapshotTests = new ClipboardSnapshotTests();
+    clipboardSnapshotTests.Run();
+    var settingsServiceTests = new SettingsServiceTests();
+    settingsServiceTests.Run();
+    var focusedContextClassifierTests = new FocusedContextClassifierTests();
+    focusedContextClassifierTests.Run();
+    var fileNameConverterTests = new FileNameConverterTests();
+    fileNameConverterTests.Run();
+    Console.WriteLine($"Passed: {tests.Passed + nativeInteropTests.Passed + inputSimulatorTests.Passed + conversionGuardTests.Passed + clipboardSnapshotTests.Passed + settingsServiceTests.Passed + focusedContextClassifierTests.Passed + fileNameConverterTests.Passed}");
+    return 0;
+}
+catch (Exception exception)
+{
+    Console.Error.WriteLine(exception);
+    return 1;
+}
